2a9663d3057a10a82163b23c93da4855b7b72ede
Diogenes D Gomes <diogenesdg@gmail.com>
2024-10-22 10:35:09 -0300

    pagina home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,31 @@
 <script>
-export default{
-    
-}
-</script>
-<template>
-    <img class="img" src="https://firebasestorage.googleapis.com/v0/b/pet-connect-c53f1.appspot.com/o/imagens%2FlogoPet.jpeg?alt=media&token=f4ab3865-7d8f-4293-bfcf-97ceaf42fb94">
-</template>
-<style>
-.img{
-    width: 100px;
-    height: 100px;
-}
-</style>
+export default {  
+}  
+</script>  
+<template>  
+ <div class="container">  
+ <img class="img" src="https://firebasestorage.googleapis.com/v0/b/pet-connect-c53f1.appspot.com/o/imagens%2FlogoPet.jpeg?alt=media&token=f4ab3865-7d8f-4293-bfcf-97ceaf42fb94" alt="Logo Pet">  
+ <p class="description">  
+ Este aplicativo faz parte de um projeto de TCC e visa ajudar proprietários que tiveram seus animais perdidos a encontrá-los. Através de uma interface intuitiva, facilitamos a comunicação e o compartilhamento de informações sobre pets desaparecidos, criando uma rede de apoio entre os donos e a comunidade.  
+ </p>  
+ </div>  
+</template>  
+<style>  
+.container {  
+ display: flex;  
+ flex-direction: column;  
+ align-items: center;  
+ text-align: center;  
+}  
+
+.img {  
+ width:100px;  
+ height:100px;  
+}  
+
+.description {  
+ margin-top:20px;  
+ font-size:20px;  
+ color: #333;  
+}  
+</style> 
