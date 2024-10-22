1ad95b44fc95afcb95348c04443ae24e9244f66a
Diogenes D Gomes <diogenesdg@gmail.com>
2024-10-22 10:39:46 -0300

    alterações home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,11 @@ export default {
 }  
 </script>  
 <template>  
- <div class="container">  
+ <div class="container"> 
+    <h1 class="desch1">Pet Connect</h1> 
  <img class="img" src="https://firebasestorage.googleapis.com/v0/b/pet-connect-c53f1.appspot.com/o/imagens%2FlogoPet.jpeg?alt=media&token=f4ab3865-7d8f-4293-bfcf-97ceaf42fb94" alt="Logo Pet">  
  <p class="description">  
- Este aplicativo faz parte de um projeto de TCC e visa ajudar proprietários que tiveram seus animais perdidos a encontrá-los. Através de uma interface intuitiva, facilitamos a comunicação e o compartilhamento de informações sobre pets desaparecidos, criando uma rede de apoio entre os donos e a comunidade.  
+ Este site faz parte de um aplicativo (projeto) de TCC e visa ajudar proprietários que tiveram seus animais perdidos a encontrá-los. Através de uma interface intuitiva, facilitamos a comunicação e o compartilhamento de informações sobre pets desaparecidos, criando uma rede de apoio entre os donos e a comunidade.  
  </p>  
  </div>  
 </template>  
@@ -19,13 +20,19 @@ export default {
 }  
 
 .img {  
- width:100px;  
- height:100px;  
+ width:200px;  
+ height:200px;  
+}  
+
+.desch1 {  
+ margin-top:20px;  
+ font-size:40px;  
+ color: #333;  
 }  
 
 .description {  
  margin-top:20px;  
  font-size:20px;  
  color: #333;  
-}  
+} 
 </style> 
