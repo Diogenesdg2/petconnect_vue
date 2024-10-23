790cb6c93783b7172f8ff47d2dbdeda8f0e75945
Diogenes D Gomes <diogenesdg@gmail.com>
2024-10-23 14:40:45 -0300

    correção da pagina home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@ export default {
     <h1 class="desch1">Pet Connect</h1> 
  <img class="img" src="https://firebasestorage.googleapis.com/v0/b/pet-connect-c53f1.appspot.com/o/imagens%2FlogoPet.jpeg?alt=media&token=f4ab3865-7d8f-4293-bfcf-97ceaf42fb94" alt="Logo Pet">  
  <p class="description">  
- Este site faz parte de um aplicativo (projeto) de TCC e visa ajudar proprietários que tiveram seus animais perdidos a encontrá-los. Através de uma interface intuitiva, facilitamos a comunicação e o compartilhamento de informações sobre pets desaparecidos, criando uma rede de apoio entre os donos e a comunidade.  
+ Esta página faz parte de um aplicativo (projeto) de TCC e visa ajudar tutores que tiveram seus animais perdidos a encontrá-los. Através de uma interface intuitiva, facilitamos a comunicação e o compartilhamento de informações sobre pets desaparecidos, criando uma rede de apoio entre os tutores e a comunidade.  
  </p>  
  </div>  
 </template>  
