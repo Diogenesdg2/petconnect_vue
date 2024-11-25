3d83fd9f0f4bcc47335576e7bebf161b77df568e
Diogenesdg2 <diogenesdg@gmail.com>
2024-11-25 20:16:15 -0300

    remoção borda logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -97,7 +97,7 @@ export default {
   width: 180px;
   height: 180px;
   border-radius: 50%;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  
   margin-bottom: 1.5rem;
   transition: transform 0.3s ease;
 }
